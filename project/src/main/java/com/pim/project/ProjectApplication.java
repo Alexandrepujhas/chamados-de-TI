60a03545da701ca86b8764467ee14a0d6288dcc2
Alexandre Expedito <alexandremaciell64@gmail.com>
2025-03-25 09:02:06 -0400

    foi adicinado um ola mundo

--- project/src/main/java/com/pim/project/ProjectApplication.java
+++ project/src/main/java/com/pim/project/ProjectApplication.java
@@ -2,10 +2,16 @@ package com.pim.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class ProjectApplication {
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
+        System.out.println("ola muqndo!");
     }
 }
+
